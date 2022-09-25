72d0e6d18c685e199ec3398947305f1ad45d638d
Adrian <rodriguezcadr@gmail.com>
2022-09-25 18:55:02 -0300

    modal card

--- js/const.js
+++ js/const.js
@@ -1,3 +1,4 @@
+const $body = document.querySelector("body");
 // Timer
 const $taskSelected = document.querySelector(".taskSelected");
 const $timeLeft = document.querySelector(".timeLeft");
@@ -7,6 +8,29 @@ const $day = document.querySelector(".day");
 const $month = document.querySelector(".month");
 const $year = document.querySelector(".year");
 
+const $addTaskForm = document.querySelector(".addTaskForm");
 const $addTaskInp = document.querySelector(".addTaskInp");
 const $addTaskBtn = document.querySelector(".addTaskBtn");
+
 const $myTasks = document.querySelector(".myTasks");
+const $modalContainer = document.querySelector(".modalContainer");
+
+const $openModalTask = document.querySelector(".openModalTask");
+const $closeModal = document.querySelector(".fa-x");
+
+const openModal = () => {
+	$modalContainer.style.display = "flex";
+	$body.style.position = "static";
+	$body.style.heigth = "100%";
+	$body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+	$modalContainer.style.display = "none";
+	$body.style.position = "inherit";
+	$body.style.heigth = "auto";
+	$body.style.overflow = "visible";
+};
+
+$openModalTask.addEventListener("click", openModal);
+$closeModal.addEventListener("click", closeModal);
--- js/main.js
+++ js/main.js
@@ -1,1 +1,68 @@
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// Funciones de local Storage
+let tasks = JSON.parse(localStorage.getItem("tasksList")) || [];
+
+const saveToLocalStorage = (tasksList) => {
+	localStorage.setItem("tasksList", JSON.stringify(tasksList));
+};
+
+// Funcion que arma el <li> para ser mostrado
+const renderList = (task) => {
+	return `
+        <li data-id="${task.name}">
+            <div>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+                <p>${task.name}</p>
+            </div>
+            <i class="fa-solid fa-arrow-right"></i>
+        </li>
+    `;
+};
+
+// Funcion que muestra el <li> en pantalla
+const showList = (taskList) => {
+	$myTasks.innerHTML = taskList.map((task) => renderList(task)).join("");
+};
+
+// Funcion para crear el objeto task
+const createTaskObj = (taskName) => {
+	const obj = {
+		name: taskName,
+		time: 25,
+		break: 5,
+		round: 0,
+		color: null,
+	};
+	return obj;
+};
+
+// Funcion para validar la task
+const isTaskValid = (taskName) => {
+	if (!taskName.length) {
+		alert("You can't enter empty tasks");
+		return false;
+	} else if (tasks.some((task) => task.name.toUpperCase() === taskName.toUpperCase())) {
+		alert("That task already exists");
+		return false;
+	}
+	return true;
+};
+
+// Funcion para añadir una tarea al taskManager
+const addTask = (e) => {
+	e.preventDefault();
+	const taskName = $addTaskInp.value.trim();
+	if (!isTaskValid(taskName)) return;
+
+	const taskObj = createTaskObj(taskName);
+	tasks = [...tasks, taskObj];
+	saveToLocalStorage(tasks);
+	showList(tasks);
+
+	input.value = "";
+};
+
+const init = () => {
+	showList(tasks);
+	$addTaskForm.addEventListener("submit", addTask);
+};
+init();
