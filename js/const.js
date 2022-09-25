cd8657d9e46b5565138a4bfba0f7ebf036d3b680
Adrian <rodriguezcadr@gmail.com>
2022-09-25 16:39:17 -0300

    first styles

---
+++ js/const.js
@@ -0,0 +1,12 @@
+// Timer
+const $taskSelected = document.querySelector(".taskSelected");
+const $timeLeft = document.querySelector(".timeLeft");
+
+// Task Manager
+const $day = document.querySelector(".day");
+const $month = document.querySelector(".month");
+const $year = document.querySelector(".year");
+
+const $addTaskInp = document.querySelector(".addTaskInp");
+const $addTaskBtn = document.querySelector(".addTaskBtn");
+const $myTasks = document.querySelector(".myTasks");
